b44cc3cb000bb4be2e1296ec866104750d37788d
Nate Bailey <nabaile586@gmail.com>
2025-05-21 14:07:58 -0400

    [#47] teehee pt2

---
+++ src/main/java/com/engine/rendering/drawings/CycleAnimateable.java
@@ -0,0 +1,28 @@
+package com.engine.rendering.drawings;
+
+import java.awt.Graphics;
+
+import com.engine.util.Image;
+import com.engine.util.Point;
+
+public class CycleAnimateable extends Animateable {
+    private final double period;
+    private double time;
+
+    public CycleAnimateable(Point position, double period, Image... frames) {
+        super(position, frames);
+
+        this.period = period;
+        this.time = 0;
+    }
+
+    public CycleAnimateable(double x, double y, double period, Image... frames) {
+        this(new Point(x, y), period, frames);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        super.draw(graphics);
+    }
+
+}
---
+++ src/main/java/com/engine/rendering/drawings/InstanceAnimateable.java
@@ -0,0 +1,57 @@
+package com.engine.rendering.drawings;
+
+import java.awt.Graphics;
+
+import com.engine.util.Image;
+import com.engine.util.Point;
+
+public class InstanceAnimateable extends Animateable {
+    private final Sprite defaultSprite;
+
+    private final double period;
+    private double time;
+    private int loops;
+    private boolean running;
+
+    public InstanceAnimateable(Point position, double period, Image defaultSprite, Image... frames) {
+        super(position, frames);
+
+        this.defaultSprite = new Sprite(position, defaultSprite);
+
+        this.period = period;
+        this.time = 0;
+        this.loops = 0;
+        this.running = false;
+    }
+
+    public InstanceAnimateable(double x, double y, double period, Image defaultSprite, Image... frames) {
+        this(new Point(x, y), period, defaultSprite, frames);
+    }
+
+    public InstanceAnimateable(Point positon, double period, Image... frames) {
+        this(positon, period, new Image(), frames);
+    }
+
+    public void run() {
+        running = true;
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        if (running) {
+            time++;
+            if (time >= period) {
+                time = 0;
+                loops++;
+                
+                nextFrame();
+            }
+            super.draw(graphics);
+        }
+        else {
+            reset();
+            defaultSprite.draw(graphics);
+        }
+    }
+
+}
