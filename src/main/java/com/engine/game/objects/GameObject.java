70769b36d31e85ee5b25bab31041cfbbce635940
Nate Bailey <nabaile586@gmail.com>
2025-05-22 09:15:31 -0400

    [#50] stashes

--- src/main/java/com/engine/game/objects/GameObject.java
+++ src/main/java/com/engine/game/objects/GameObject.java
@@ -6,7 +6,7 @@ import com.engine.rendering.drawings.Drawable;
 import com.engine.util.Clickable;
 import com.engine.util.Updateable;
 
-public interface GameObject extends Updateable, Drawable, Collider, Clickable, INetObject {
+public interface GameObject extends Updateable, Drawable, Collider, INetObject {
     // GameObject is an interface that combines the functionalities of Drawable, Updateable, and Collidable.
     // It represents a game object that can be drawn, updated, and checked for collisions.
     // No additional methods are needed here as the interface already extends the necessary interfaces.
